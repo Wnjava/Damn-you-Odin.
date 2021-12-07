9eeef9b22ee50aaf2580ca140243b2d795d2aed8
Alex <wnjava92@gmail.com>
2021-12-08 00:20:53 +0200

    changes

--- js.js
+++ js.js
@@ -1,13 +1,18 @@
-
-let stopC;
+let x=1;
 
 function hey () {
     document.getElementById("pic").src = "exp.jpg";
-    stopC = setTimeout(alertFunc,5000);
+    setTimeout(alertFunc,5000);
 
 }
 
 function alertFunc() {
-    alert("Stop right there,criminal scum!") ;
+    x++;
     document.getElementById("pic").src="https://content1.rozetka.com.ua/goods/images/big/208130202.jpg" ;
+    
+    while (x <=2) {
+        alert("Stop right there,criminal scum!") ; {
+            break;
+    } 
+}
 }
